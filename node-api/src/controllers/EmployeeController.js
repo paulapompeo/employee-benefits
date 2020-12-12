0c06d500d122a246eb436bee50d2872563db3c3d
Paula Pompeo <paulapompeoc@gmail.com>
2020-12-12 19:29:08 -0300

    employees routes

---
+++ node-api/src/controllers/EmployeeController.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const Employee = mongoose.model('Employee');
+
+module.exports = {
+  async index(request, response) {
+    const employees = await Employee.find();
+
+    return response.json(employees);
+  },
+  
+  async store(request, response){
+    const employee = await Employee.create(request.body);
+
+    return response.json(employee);
+  },
+
+  async show(request, response){
+    const employee = await Employee.findById(request.params.id);
+
+    return response.json(employee);
+  },
+
+  async update(request, response){
+    const employee = await Employee.findByIdAndUpdate(request.params.id, request.body, { new: true });
+
+    return response.json(employee)
+  },
+
+  async destroy(request, response) {
+    await Employee.findByIdAndRemove(request.params.id);
+
+    return response.send()
+  },
+}
--- node-api/src/routes.js
+++ node-api/src/routes.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const routes = express.Router();
 
-const CompanyController = require("./controllers/CompanyController")
+const CompanyController = require("./controllers/CompanyController");
+const EmployeeController = require("./controllers/EmployeeController");
 
 routes.get('/companies', CompanyController.index);
 routes.get('/companies/:id', CompanyController.show);
 routes.post('/companies', CompanyController.store);
 routes.put('/companies/:id', CompanyController.update);
 routes.delete('/companies/:id', CompanyController.destroy);
+
+routes.get('/employees', EmployeeController.index);
+routes.get('/employees/:id', EmployeeController.show);
+routes.post('/employees', EmployeeController.store);
+routes.put('/employees/:id', EmployeeController.update);
+routes.delete('/employees/:id', EmployeeController.destroy);
 
 module.exports = routes;
 
