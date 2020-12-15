d8edfcd9beaeafc6ac5891c0742c14c6afc7187f
Paula Pompeo <paulapompeoc@gmail.com>
2020-12-15 16:56:04 -0300

    comecando o front

--- node-api/src/routes.js
+++ node-api/src/routes.js
@@ -14,44 +14,9 @@ routes.delete('/companies/:id', CompanyController.destroy);
 
 routes.get('/employees', EmployeeController.index);
 routes.get('/employees/:id', EmployeeController.show);
-routes.post('/employees', EmployeeController.store);
+// routes.post('/employees', EmployeeController.store);
 // routes.post('/companies/:id/employees', EmployeeController.store);
 routes.put('/employees/:id', EmployeeController.update);
 routes.delete('/employees/:id', EmployeeController.destroy);
 
 module.exports = routes;
-
-
-// const mongoose = require('mongoose');
-// const Company = mongoose.model('Company');
-
-// routes.get('/companies', (request, response) => {
-//   Company.create({
-//       name: "Teste do Array",
-//       trade: "Como um Flash",
-//       cnpj: 123456,
-//       address: "Rua das Flores",
-//       benefits: [1, 2, 3, 4, 5, 6]
-//       });
-
-//   return response.send("foi")
-// });
-
-// module.exports = routes;
-
-
-      // Company.create({
-    //   name: "Flash 2",
-    //   trade: "Como um Flash",
-    //   cnpj: 123456,
-    //   address: "Rua das Flores",
-    //   benefits: [1, 2, 3, 4]
-    //   });
-  
-    // Employee.create({
-    //   company: "5fd432a02e413a0eaf385f7f",
-    //   name: "Livia",
-    //   lastName: "Pompeo",
-    //   cpf: 1234567,
-    //   email: "paulapompo@gmail.com"
-    //   });
---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h1>Beneficios da Flash</h1>
+  );
+}
+
+export default App;
