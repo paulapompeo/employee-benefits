f62f2fc4a2ba9c0b299746b1fb8f2557bca27e1f
Paula Pompeo <paulapompeoc@gmail.com>
2020-12-15 18:24:31 -0300

    estilo global

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -1,1 +1,32 @@
 import { createGlobalStyle } from 'styled-components';
+
+import shape from '../assets/shape.svg';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #EFFAFA url(${shape}) no-repeat;
+    background-size: cover;
+    --webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 16px Spartan, sans-serif;
+  }
+
+  #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
